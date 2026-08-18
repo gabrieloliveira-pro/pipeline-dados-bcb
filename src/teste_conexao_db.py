6b971a16eb348203ac83f358aaf9944ab02ec571
gabriel oliveira <julioogabriel24@gmail.com>
2026-08-18 18:50:32 -0300

    feat: adiciona teste de conexão com o PostgreSQL via .env

---
+++ src/teste_conexao_db.py
@@ -0,0 +1,19 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+try:
+    conexao = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+
+    print('Conexão com o PostgreeSQL estabelecida com sucesso!')
+    conexao.close()
+except Exception as e:
+    print(f'Erro ao conectar: {e}')
